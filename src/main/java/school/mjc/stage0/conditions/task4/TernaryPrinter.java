dee5046313c6faebc2eb43fb768bef92bd44c96a
nkere30 <ninokereselidze30@gmail.com>
2022-08-06 21:10:48 +0400

    Completed

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public static void printWhichIsBigger(int first, int second) {
-        System.out.println(first > second? first : second);
+        System.out.println(first > second? "first" : "second");
     }
     public static void main(String[] args) {
         printWhichIsBigger(1, 5);
